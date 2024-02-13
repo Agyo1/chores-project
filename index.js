760d1a1ca49e42611b366083da0a37ef5aa8cca1
Jason Lord <jtl7690@gmail.com>
2024-02-12 20:18:41 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+  // Select all elements with the class "random-element"
+  const elements = document.querySelectorAll(".page");
+
+  // Iterate over each element and assign random left and top CSS properties
+  elements.forEach(function (element) {
+    const left = Math.floor(Math.random() * 71) + 10; // Random left position
+    const top = Math.floor(Math.random() * 71) + 10; // Random top position
+
+    // Set the left and top CSS properties
+    element.style.left = left + "vw";
+    element.style.top = top + "vh";
+  });
+};
+
+function showDoc() {
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+
+    // Display the Google Document in the iframe
+    const docFrame = document.getElementById("docFrame");
+    docFrame.src = "https://docs.google.com/document/d/1XaV-PWbP9bbIkwW3JM51Ds5FBlBJXXoYfT0c8Ny0MOs/edit?usp=sharing";
+}
+function closeFrame() {
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+}
